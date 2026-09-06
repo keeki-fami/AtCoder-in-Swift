12fad51c563b44dee095a6ccb12ec7b11e0b4653
keeki-fami <towatowa3939keeki@gmail.com>
2026-09-06 12:00:22 +0900

    add subscript

---
+++ Sources/AtCoderInSwift/Algo/PrefixSum/PrefixSum.swift
@@ -0,0 +1,22 @@
+public struct PrefixSum {
+	private var _list: [Int]
+	init(_ list: [Int]) {
+		let n = list.count
+		self._list = [0] + list
+		for i in 1...n {
+			self._list[i] = self._list[i-1]
+		}
+	}
+
+	public subscript(i: Int, j: Int) -> Int {
+		get {
+			return self._list[j] - self._list[i]
+		}
+	}
+
+	public subscript(i: Int) -> Int {
+		get {
+			return self._list[i]
+		}
+	}
+}
