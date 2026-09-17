c9c4f81a75df0b4552c9592c1f8d08f86d6f09bc
keeki-fami <towatowa3939keeki@gmail.com>
2026-09-17 16:54:52 +0900

    add FastOutput

--- Sources/AtCoderInSwift/Output/output.swift
+++ Sources/AtCoderInSwift/Output/output.swift
@@ -32,8 +32,24 @@ public func customPrintTF(_ judge: () -> Bool, t: String, f: String) {
 	}
 }
 
+public func customPrintTF(_ flag: Bool, t: String, f: String) {
+	if flag {
+		print(t)
+	} else {
+		print(f)
+	}
+}
+
 public func printYN(_ judge: () -> Bool) {
 	if judge() {
+		print("Yes")
+	} else {
+		print("No")
+	}
+}
+
+public func printYN(_ flag: Bool) {
+	if flag {
 		print("Yes")
 	} else {
 		print("No")
@@ -46,4 +62,30 @@ public func printY() {
 
 public func printN() {
 	print("No")
+}
+
+@MainActor
+struct FastOutput {
+	static var string = ""
+	static func add(_ str: String, _ separator: String = "") {
+		string += (str + separator)
+	}
+
+	static func addLine(_ str: String, _ separator: String = "") {
+		string += (str + separator)
+		string += "\n"
+	}
+
+	static func add(_ int: Int, _ separator: String = "") {
+		string += ("\(int)" + separator)
+	}
+
+	static func addLine(_ int: Int, _ separator: String = "") {
+		string += ("\(int)" + separator)
+		string += "\n"
+	}
+	static func out() {
+		print(string, separator: "")
+		string = ""
+	}
 }
